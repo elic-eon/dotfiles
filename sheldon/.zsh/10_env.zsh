973b0935bf458e164a6fb5fd8b43c8d0ffc1563d
ssuyi <elic.space.cs@gmail.com>
2025-09-27 23:51:43 +0800

    fix order

--- sheldon/.zsh/10_env.zsh
+++ sheldon/.zsh/10_env.zsh
@@ -1,11 +1,5 @@
 export EDITOR=vim
 
-# set cache env for OMZ completion plugins
-export ZSH_CACHE_DIR="$HOME/.cache/zsh"
-
-# include completion functions
-export FPATH="$ZSH_CACHE_DIR/completions:$FPATH"
-
 # set history
 export HISTFILE="$HOME/.history"
 export HISTSIZE=1000000
